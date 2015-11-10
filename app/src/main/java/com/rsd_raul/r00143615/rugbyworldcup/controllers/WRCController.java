b82398e0a3e839e52af34494f4fb1cf561131d76
rsd-raul <rsd_raul@yahoo.es>
2015-11-10 09:40:31 +0000

    Code cleanup and minor corrections

--- app/src/main/java/com/rsd_raul/r00143615/rugbyworldcup/controllers/WRCController.java
+++ app/src/main/java/com/rsd_raul/r00143615/rugbyworldcup/controllers/WRCController.java
@@ -2,10 +2,19 @@ package com.rsd_raul.r00143615.rugbyworldcup.controllers;/*
  * Created by rsd_raul on 11/9/15.
  */
 
+import android.support.v4.content.ContextCompat;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.rsd_raul.r00143615.rugbyworldcup.Knockout;
+import com.rsd_raul.r00143615.rugbyworldcup.R;
 import com.rsd_raul.r00143615.rugbyworldcup.domain.Team;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class WRCController {
 
@@ -15,7 +24,7 @@ public class WRCController {
         this.activity = activity;
     }
 
-    private boolean isValidTeam() {
+    public boolean isValidTeam() {
         EditText editText = (EditText) activity.getCurrentFocus();
         Team actual = null;
         boolean result = false;
@@ -28,18 +37,102 @@ public class WRCController {
 
             //  If exists change background and check the rest for unlocking the button
             if(actual != null) {
-                editText.setBackgroundColor(activity.getResources().getColor(actual.getColor()));
-                editText.setTextColor(activity.getResources().getColor(android.R.color.white));
+
+                editText.setBackgroundColor(ContextCompat.getColor(activity, actual.getColor()));
+                editText.setTextColor(ContextCompat.getColor(activity, android.R.color.white));
                 allTeamsValid();
                 result = true;
                 //  If not, go to default
             }else{
-                editText.setBackgroundColor(activity.getResources().getColor(android.R.color.white));
-                editText.setTextColor(activity.getResources().getColor(android.R.color.black));
+                editText.setBackgroundColor(ContextCompat.getColor(activity, android.R.color.white));
+                editText.setTextColor(ContextCompat.getColor(activity, android.R.color.black));
                 activity.toggleButton(false);
             }
         }
 
         return result;
     }
+
+    public boolean isValidOnParents(){
+        EditText editText = (EditText) activity.getCurrentFocus();
+        Team actual = activity.getTeam(editText);
+        boolean result = true;
+        Team daddy;
+        Team mommy;
+
+        switch (editText.getId()){
+            case R.id.semi1:
+                daddy = activity.getTeam((EditText) activity.findViewById(R.id.quarter1));
+                mommy = activity.getTeam((EditText) activity.findViewById(R.id.quarter2));
+
+                result = testParent(daddy, mommy, actual);
+                break;
+            case R.id.semi2:
+                daddy = activity.getTeam((EditText) activity.findViewById(R.id.quarter3));
+                mommy = activity.getTeam((EditText) activity.findViewById(R.id.quarter4));
+
+                result = testParent(daddy, mommy, actual);
+                break;
+            case R.id.semi3:
+                daddy = activity.getTeam((EditText) activity.findViewById(R.id.quarter5));
+                mommy = activity.getTeam((EditText) activity.findViewById(R.id.quarter6));
+
+                result = testParent(daddy, mommy, actual);
+                break;
+            case R.id.semi4:
+                daddy = activity.getTeam((EditText) activity.findViewById(R.id.quarter7));
+                mommy = activity.getTeam((EditText) activity.findViewById(R.id.quarter8));
+
+                result = testParent(daddy, mommy, actual);
+                break;
+            case R.id.final1:
+                daddy = activity.getTeam((EditText) activity.findViewById(R.id.semi1));
+                mommy = activity.getTeam((EditText) activity.findViewById(R.id.semi2));
+
+                result = testParent(daddy, mommy, actual);
+                break;
+            case R.id.final2:
+                daddy = activity.getTeam((EditText) activity.findViewById(R.id.semi3));
+                mommy = activity.getTeam((EditText) activity.findViewById(R.id.semi4));
+
+                result = testParent(daddy, mommy, actual);
+                break;
+            case R.id.winner:
+                daddy = activity.getTeam((EditText) activity.findViewById(R.id.final1));
+                mommy = activity.getTeam((EditText) activity.findViewById(R.id.final2));
+
+                result = testParent(daddy, mommy, actual);
+                break;
+        }
+
+        return result;
+    }
+
+    public boolean testParent(Team daddy, Team mommy, Team actual) {
+        Boolean result = true;
+        if(actual != daddy && actual != mommy) {
+            Toast.makeText(activity, R.string.wrongParent, Toast.LENGTH_SHORT).show();
+            result = false;
+        }
+        return result;
+    }
+
+    public void allTeamsValid() {
+        List<Team> teams = activity.getTypedTeams();
+
+        //  Duplicate detection
+        List<Team> teamsCopy = new ArrayList<>(teams);
+        teamsCopy.removeAll(Collections.singleton(null));
+        Set<Team> setTeams = new HashSet<>(teamsCopy);
+        Boolean duplicates = teamsCopy.size() != setTeams.size();
+
+        if(duplicates) {
+            Toast.makeText(activity, R.string.duplicatedTeam, Toast.LENGTH_SHORT).show();
+        }else if(teams.contains(null) || duplicates)
+            activity.toggleButton(false);
+        else
+            activity.toggleButton(true);
+    }
+
+
 }
